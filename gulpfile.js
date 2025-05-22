b7a70bdc2da7ce2367bcd7d8a4bb668c427b0645
jose villegas <josevillegas13600@gmail.com>
2025-05-22 18:01:35 -0400

    compilarsasscongulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,13 @@
-export function hola(done){
-    console.log('Hola desde gulpfile.js')
+import {src, dest} from 'gulp'
+import * as dartSass from 'sass'
+import gulpSaas from 'gulp-sass'
+
+const sass =gulpSaas(dartSass)
+
+export function css(done)
+{
+    src('src/scss/app.scss')
+        .pipe(sass())
+        .pipe(dest('build/css'))
     done()
 }
